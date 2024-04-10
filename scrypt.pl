5ece9adaf21329bbd4796a0db66f86af9509ddf0
t1011 <31567272+t1011@users.noreply.github.com>
2024-04-10 19:50:47 +0300

    Create scrypt.pl

---
+++ scrypt.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $REGEXP = shift || die "no email-adress given (regexp-style, e.g. bl.*\@yahoo.com)!";
+
+my @data = qx</usr/sbin/postqueue -p>;
+my %Q;
+my $queue_id;
+
+for (@data) {
+  if (/^(\w+)(\*|\!)?\s/) {
+     $queue_id = $1;
+  }
+  if($queue_id && /$REGEXP/i) {
+    $Q{$queue_id} = 1;
+    $queue_id = "";
+  }
+}
+
+open(my $POSTSUPER,"|postsuper -d -") || die "couldn't open postsuper" ;
+
+foreach (keys %Q) {
+  print $POSTSUPER "$_\n";
+}
+close($POSTSUPER);
